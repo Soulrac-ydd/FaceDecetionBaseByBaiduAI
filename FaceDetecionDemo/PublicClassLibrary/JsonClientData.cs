39a458b9cabc027f690bd27e810fe785a06e572c
庭豪 胡 <652157227@qq.com>
2019-06-26 15:18:33 +0800

    Get the distance between upper lip and lower lip(Offline)

---
+++ FaceDetecionDemo/PublicClassLibrary/JsonClientData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicClassLibrary
+{
+    public class JsonClientData
+    {
+        public string refresh_token { get; set; }
+
+        public long expires_in { get; set; }
+
+        public string session_key { get; set; }
+
+        public string access_token { get; set; }
+
+        public string scope { get; set; }
+
+        public string session_secret { get; set; }
+    }
+}
---
+++ FaceDetecionDemo/PublicClassLibrary/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicClassLibrary
+{
+    public class Position
+    {
+        public double x { get; set; }
+
+        public double y { get; set; }
+
+        public Position(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
